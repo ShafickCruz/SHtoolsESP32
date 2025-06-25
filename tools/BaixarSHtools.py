f62b2049a07bcf98dfd0e538d87996b64d3d8b97
Shafick Cruz <shafick@outlook.com.br>
2025-06-25 03:50:32 +0100

    teste

--- tools/BaixarSHtools.py
+++ tools/BaixarSHtools.py
@@ -13,15 +13,15 @@ if not os.path.exists(repo_dir):
 else:
     print("SHtools já está presente. Nenhuma ação necessária.")
 
-# Copia as libs internas de SHtoolsESP32/lib/ para lib/ do projeto, se não existirem
+# Move as libs internas de SHtoolsESP32/lib/ para lib/ do projeto, se não existirem
 src_libs_dir = os.path.join(repo_dir, "lib")
 if os.path.exists(src_libs_dir):
     for lib_name in os.listdir(src_libs_dir):
         lib_path = os.path.join(src_libs_dir, lib_name)
         target_path = os.path.join(proj_lib_dir, lib_name)
         if os.path.isdir(lib_path) and not os.path.exists(target_path):
-            print(f"Copiando {lib_name} para {target_path}...")
-            shutil.copytree(lib_path, target_path)
+            print(f"Movendo {lib_name} para {target_path}...")
+            shutil.move(lib_path, target_path)
         else:
             print(f"{lib_name} já existe em {target_path}. Nenhuma ação.")
 else:
